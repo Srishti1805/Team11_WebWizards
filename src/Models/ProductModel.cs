4baa61bf304e95ab6d23dd0517d551212027cfdc
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2023-10-04 15:06:29 -0700

    feat: added new column names in the product model

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -6,17 +6,20 @@ namespace ContosoCrafts.WebSite.Models
     public class ProductModel
     {
         public string Id { get; set; }
-        public string Maker { get; set; }
-        
+        public string Owner { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
         [JsonPropertyName("img")]
         public string Image { get; set; }
         public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string review { get; set; }
         public int[] Ratings { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
- 
+
     }
 }
