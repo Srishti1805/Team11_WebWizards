5725fdf688ec5256d9bcd445f3b778815eab423e
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2023-10-19 20:57:00 -0700

    Added comments to Program.cs as per coding standards

--- src/Program.cs
+++ src/Program.cs
@@ -3,25 +3,13 @@ using Microsoft.Extensions.Hosting;
 
 namespace ContosoCrafts.WebSite
 {
-    /// <summary>
-    /// The Program class contains the entry point of the application.
-    /// </summary>
     public class Program
     {
-        /// <summary>
-        /// The Main method is the entry point for the application.
-        /// </summary>
-        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            // Build and run the host.
             CreateHostBuilder(args).Build().Run();
         }
-        /// <summary>
-        /// CreateHostBuilder is used to configure and build the application host.
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
