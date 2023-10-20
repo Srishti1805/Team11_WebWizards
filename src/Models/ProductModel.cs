beaadfdef451590cb393750822e12c9866d7d12f
RupaDineshKarumanchi <129802431+RupaDineshKarumanchi@users.noreply.github.com>
2023-10-20 15:28:55 -0700

    Added comments to the aspects of persoanl information of the product to the file ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -8,9 +8,27 @@ namespace ContosoCrafts.WebSite.Models
     /// </summary>
     public class ProductModel
     {
+        // Properties representing various aspects of a product
+
+        /// <summary>
+        /// Gets or sets the unique identifier of the product.
+        /// </summary>
         public string Id { get; set; }
+        /// <summary>
+        /// Gets or sets the owner of the product.
+        /// </summary>
         public string Owner { get; set; }
+        /// <summary>
+        /// Gets or sets the phone number associated with the product.
+        /// </summary>
         public string Phone { get; set; }
+        /// <summary>
+        /// Gets or sets the name associated with the product.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the email address associated with the product.
+        /// </summary>
         public string Email { get; set; }
 
         [JsonPropertyName("img")]
