9007856c061546513d2186a899bd0575dfd7f647
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2023-10-20 18:51:38 -0700

    Added comments to Index.cshtml.cs file as per the coding standards

--- src/Pages/Index.cshtml.cs
+++ src/Pages/Index.cshtml.cs
@@ -24,20 +24,32 @@ namespace ContosoCrafts.WebSite.Pages
     ///John Vikas Kotaru
     ///</summary>
 
+
+    /// <summary>
+    /// The IndexModel represents the Razor Pages model for the index page.
+    /// It handles the display of products on the homepage.
+    /// </summary>
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
 
+        /// Constructor for IndexModel
         public IndexModel(ILogger<IndexModel> logger,
             JsonFileProductService productService)
         {
             _logger = logger;
             ProductService = productService;
         }
+ 
+        // Gets the product service used to fetch product data.
 
         public JsonFileProductService ProductService { get; }
+
+        /// Gets or sets the list of products to be displayed on the index page.
         public IEnumerable<ProductModel> Products { get; private set; }
 
+        // Handles the HTTP GET request for the index page.
+        // Retrieves product data using the ProductService and sets the Products property.
         public void OnGet()
         {
             Products = ProductService.GetProducts();
