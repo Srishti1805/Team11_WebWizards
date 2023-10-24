8b558408e3f7c84509dba352c818aac4c78a9c7f
John Vikas Kotaru <jkotaru@seattleu.edu>
2023-10-23 23:54:04 -0700

    Added new test Update.cshtml.Tests.cs file for unit testing update page.

---
+++ UnitTests/Pages/Product/Update.cshtml.Tests.cs
@@ -0,0 +1,96 @@
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Services;
+
+namespace UnitTests.Pages.Product.Update
+{
+    /// <summary>
+    /// This class contains unit tests for the UpdateModel class in the "Pages/Product/Update" namespace.
+    /// </summary>
+    public class UpdateTests
+    {
+        #region TestSetup
+        public static IUrlHelperFactory urlHelperFactory;
+        public static DefaultHttpContext httpContextDefault;
+        public static IWebHostEnvironment webHostEnvironment;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
+        public static PageContext pageContext;
+
+        public static UpdateModel pageModel;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            httpContextDefault = new DefaultHttpContext()
+            {
+                //RequestServices = serviceProviderMock.Object,
+            };
+
+            modelState = new ModelStateDictionary();
+
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData,
+            };
+
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var MockLoggerDirect = Mock.Of<ILogger<IndexModel>>();
+            JsonFileProductService productService;
+
+            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
+
+            pageModel = new UpdateModel(productService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        #region OnPost
+        [Test]
+        public void OnPost_Valid_Should_Return_Product_Price()
+        {
+            // Arrange
+            pageModel.OnGet("Gardening-gridder");
+            string temp = pageModel.Product.Price;
+
+            // Act
+            pageModel.Product.Price = "20";
+            pageModel.OnPost();
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual("20", pageModel.Product.Price);
+            pageModel.Product.Price = temp;
+        }
+        #endregion OnPost
+    }
+}
