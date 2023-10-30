1bf71db884cbb824ecefad617c560eed5fd3bce6
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2023-10-30 11:28:54 -0700

    Added comments in OnGet() function in Error.cshtml.Tests.cs page

--- UnitTests/Pages/Error.cshtml.Tests.cs
+++ UnitTests/Pages/Error.cshtml.Tests.cs
@@ -39,16 +39,24 @@ namespace UnitTests.Pages.Error
 
         public static ErrorModel pageModel;
 
+        /// <summary>
+        /// Initializes the necessary services and parameters for the test.
+        /// </summary>
         [SetUp]
         public void TestInitialize()
         {
+            // Setting up the HttpContext, ActionContext, ViewData, TempData, and other required components
+            // for simulating a page request.
             httpContextDefault = new DefaultHttpContext()
             {
                 TraceIdentifier = "trace",
                 //RequestServices = serviceProviderMock.Object,
             };
+            // Arrange necessary context and services for the test.
             httpContextDefault.HttpContext.TraceIdentifier = "trace";
 
+            // Act
+            // Initialize the ErrorModel and related components.
             modelState = new ModelStateDictionary();
 
             actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
@@ -63,6 +71,8 @@ namespace UnitTests.Pages.Error
                 HttpContext = httpContextDefault
             };
 
+            // Assert
+            // Ensure that the test components are properly initialized.
             var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
             mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
             mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
@@ -82,39 +92,53 @@ namespace UnitTests.Pages.Error
 
         #endregion TestSetup
 
+        /// <summary>
+        /// Unit test method to validate the behavior of the OnGet method when activity is valid.
+        /// </summary>
         #region OnGet
         [Test]
         public void OnGet_Valid_Activity_Set_Should_Return_RequestId()
         {
             // Arrange
-
+            // Start a new activity for testing purposes
             Activity activity = new Activity("activity");
             activity.Start();
 
             // Act
+            // Call the OnGet method to simulate the GET request.
             pageModel.OnGet();
 
             // Reset
+            // Stop the activity after the test.
             activity.Stop();
 
             // Assert
+            // Ensure that the ModelState of the pageModel is valid after executing OnGet.
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            // Ensure that the RequestId in pageModel matches the Id of the started activity.
             Assert.AreEqual(activity.Id, pageModel.RequestId);
         }
 
+        /// <summary>
+        /// Unit test method to validate the behavior of the OnGet method when activity is null.
+        /// </summary>
         [Test]
         public void OnGet_InValid_Activity_Null_Should_Return_TraceIdentifier()
         {
             // Arrange
 
             // Act
+            // Call the OnGet method to simulate the GET request.
             pageModel.OnGet();
 
             // Reset
 
             // Assert
+            // Ensure that the ModelState of the pageModel is valid after executing OnGet.
             Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            // Ensure that the RequestId in pageModel matches the TraceIdentifier of the HttpContext.
             Assert.AreEqual("trace", pageModel.RequestId);
+            // Ensure that ShowRequestId property is set to true.
             Assert.AreEqual(true, pageModel.ShowRequestId);
         }
         #endregion OnGet
