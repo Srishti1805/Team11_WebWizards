0b83f2d2b2189c00d3690a7a5b11a60b6ab95991
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2023-10-30 14:07:54 -0700

    Added comments in AboutUs.cshtml.cs page accoeding to the coding standards

--- src/Pages/AboutUs.cshtml.cs
+++ src/Pages/AboutUs.cshtml.cs
@@ -3,11 +3,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
-{
+{    /// <summary>
+     /// This is the model for the "About Us" page.
+     /// </summary>
     public class About_UsModel : PageModel
     {
+        /// <summary>
+        /// This method is executed when the HTTP GET request is made to the "About Us" page.
+        /// </summary>
 		public void OnGet()
         {
+            // Implementation for handling the GET request can be added here.
         }
     }
 }
