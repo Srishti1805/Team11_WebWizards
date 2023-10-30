3eb4368b834c57c80438207a874b87b182d780ff
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2023-10-30 14:12:16 -0700

    Added comments  to Error.cshtml.cs page accoding to coding standards

--- src/Pages/Error.cshtml.cs
+++ src/Pages/Error.cshtml.cs
@@ -4,22 +4,40 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
+
+
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Represents the model for the error page.
+    /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the request identifier.
+        /// </summary>
+        public string RequestId { get; set; }
+        /// <summary>
+        /// Gets a value indicating whether to show the request identifier.
+        /// </summary>
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
         private readonly ILogger<ErrorModel> _logger;
 
+        /// <summary>
+        /// Initializes a new instance of the ErrorModel class.
+        /// </summary>
+        /// <param name="logger">The logger used for error logging.</param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             _logger = logger;
         }
-
+        /// <summary>
+        /// This method is executed when the HTTP GET request is made to the error page.
+        /// It sets the RequestId based on the current activity or trace identifier.
+        /// </summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
