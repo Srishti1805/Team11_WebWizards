a311439544ce2bb65f18964129329c5ae550fab2
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2023-10-30 14:17:31 -0700

    Added comments to Privacy.cshtml.cs according to the coding standards

--- src/Pages/Privacy.cshtml.cs
+++ src/Pages/Privacy.cshtml.cs
@@ -3,17 +3,27 @@ using Microsoft.Extensions.Logging;
 
 namespace ContosoCrafts.WebSite.Pages
 {
+    /// <summary>
+    /// Represents the model for the Privacy page.
+    /// </summary>
     public class PrivacyModel : PageModel
     {
+        /// <summary>
+        /// Initializes a new instance of the PrivacyModel class.
+        /// </summary>
+        /// <param name="logger">The logger used for logging in the PrivacyModel.</param>
         private readonly ILogger<PrivacyModel> _logger;
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
             _logger = logger;
         }
-
+        /// <summary>
+        /// This method is executed when the HTTP GET request is made to the Privacy page.
+        /// </summary>
         public void OnGet()
         {
+            // Implementation for handling the GET request can be added here if needed.
         }
     }
 }
