d84b7b9a68ef7ec9436d5650dd42940560aff606
John Vikas Kotaru <jkotaru@seattleu.edu>
2023-11-07 13:09:07 -0800

    Create new ProductsController.Tests.cs file

---
+++ UnitTests/Controllers/ProductsController.Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using NUnit.Framework;
+
+namespace UnitTests.Model.Tests
+{
+    /// <summary>
+    /// This class contains unit tests for the ProductModel
+    /// </summary>
+    public class ProductModelTests
+    {
+        private const string MockId = "1";
+
+        private const string MockTitle = "Gridder";
+
+        private const string MockOwner = "Srishti";
+
+        private const string MockDescription = "The Gridder is a gardening tool that rolls out perfectly spaced grids for planting.";
+
+        private const string MockUrl = "https://www.youtube.com/watch?v=SxygEWYwAB8";
+
+        private const string MockImage = "https://i.pinimg.com/564x/b2/4a/4e/b24a4e95612802454143fa54fd999741.jpg";
+
+        private const string MockLocation = "Seattle, Redmond, Belleve";
+
+        [Test]
+        public void ToString_ReturnsJson()
+        {
+            // Arrange
+            var product = new ProductModel
+            {
+                Id = MockId,
+                Title = MockTitle,
+                Owner = MockOwner,
+                Url = MockUrl,
+                Image = MockImage,
+                Location = MockLocation
+            };
+
+            // Act
+            var jsonString = product.ToString();
+
+            // Assert
+            Assert.IsNotEmpty(jsonString);
+            Assert.IsTrue(jsonString.Contains(product.Id));
+            Assert.IsTrue(jsonString.Contains(product.Title));
+            Assert.IsTrue(jsonString.Contains(product.Owner));
+            Assert.IsTrue(jsonString.Contains(product.Url));
+            Assert.IsTrue(jsonString.Contains(product.Image));
+            Assert.IsTrue(jsonString.Contains(product.Location));
+
+        }
+    }
+}
