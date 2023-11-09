9a2783c12fe41da648f871827d834109f6062e90
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2023-11-09 14:02:56 -0800

    Added a c# file index_gardening.cshtml.cs file

---
+++ src/Pages/Product/Index_gardening.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class Index_gardeningModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
