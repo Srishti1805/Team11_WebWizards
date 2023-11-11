6253fc0a8c560a70c75d5995ec2bc67c08edd4f8
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2023-11-11 09:19:27 -0800

    Added Products function in Index_SonwEquipment.cshtml.cs page

--- src/Pages/Product/Index_SnowEquipment.cshtml.cs
+++ src/Pages/Product/Index_SnowEquipment.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +11,20 @@ namespace ContosoCrafts.WebSite.Pages.Product
 {
 	public class Index_SnowEquipmentModel : PageModel
     {
+        public Index_SnowEquipmentModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+
+        // Collection of the Data
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
+
         public void OnGet()
         {
         }
