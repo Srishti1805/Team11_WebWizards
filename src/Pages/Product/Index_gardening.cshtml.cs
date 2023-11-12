24cb96479602c7160f176db3c54345dc9de61731
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2023-11-11 22:36:39 -0800

    Added comments in Index_gardening.cshtml.cs page

--- src/Pages/Product/Index_gardening.cshtml.cs
+++ src/Pages/Product/Index_gardening.cshtml.cs
@@ -3,16 +3,21 @@ using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 
- 
+// Namespace for Product related to the Index page
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// Index Page will return all the data to show
+    /// </summary>
     public class Index_gardeningModel : PageModel
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
         public Index_gardeningModel(JsonFileProductService productService)
         {
-
             ProductService = productService;
-
         }
 
         // Data Service
