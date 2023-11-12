112955656b8a711040ea532fb8b56f7615c3e57d
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2023-11-12 14:34:53 -0800

    Updated OnGet() function in Index_SnowEquipment.cshtml.cs page

--- src/Pages/Product/Index_SnowEquipment.cshtml.cs
+++ src/Pages/Product/Index_SnowEquipment.cshtml.cs
@@ -27,7 +27,7 @@ namespace ContosoCrafts.WebSite.Pages.Product
 
         public void OnGet()
         {
-            Products = ProductService.GetFirstTwoProducts();
+            Products = ProductService.GetLastTwoProducts();
         }
     }
 }
