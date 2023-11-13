6b2a8a39632f5db95a4d5d9327e6158aa0795e22
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2023-11-13 00:02:46 -0800

    Added Index_gardening Model in Index_camping.cshtml.cs

--- src/Pages/Product/Index_camping.cshtml.cs
+++ src/Pages/Product/Index_camping.cshtml.cs
@@ -1,12 +1,43 @@
-using Microsoft.AspNetCore.Mvc;
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
+// Namespace for Product related to the Index page
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// Index Page will return all the data to show
+    /// </summary>
     public class Index_campingModel : PageModel
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public Index_campingModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+        // Collection of the Data
+
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
+
         public void OnGet()
         {
+            Products = ProductService.GetLastTwoProducts();
         }
+
     }
+
 }
+
+
+
