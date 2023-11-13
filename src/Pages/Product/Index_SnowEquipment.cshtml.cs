2d8373f09610dd4afc757d6c690665ddf20cd6b9
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2023-11-13 09:47:26 -0800

    Added comments to OnGet() function in Index_SnowEquipment.cs page

--- src/Pages/Product/Index_SnowEquipment.cshtml.cs
+++ src/Pages/Product/Index_SnowEquipment.cshtml.cs
@@ -10,26 +10,36 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 // Namespace declaration for the current class
 namespace ContosoCrafts.WebSite.Pages.Product
 {
-    // Declaration of the Razor Pages model class, inheriting from PageModel
+    /// <summary>
+    /// Declaration of the Razor Pages model class, inheriting from PageModel
+    /// </summary>
     public class Index_SnowEquipmentModel : PageModel
     {
         // Constructor for the class, accepting a JsonFileProductService instance as a dependency
         public Index_SnowEquipmentModel(JsonFileProductService productService)
         {
+            // Initializing the ProductService property with the provided instance
             ProductService = productService;
         }
-        // Data Service
+        // Declaration of the data service property
+        // It holds an instance of JsonFileProductService
         public JsonFileProductService ProductService { get; }
 
-        // Collection of the Data
+        /// <summary>
+        /// Declaration of the property to hold a collection of ProductModel objects
+        // It is settable only within the class (private set;)
+        /// </summary>
         public IEnumerable<ProductModel> Products { get; private set; }
 
         /// <summary>
-        /// REST OnGet, return all data
+        /// HTTP GET handler for the page.
+        /// Retrieves products with the category "Snow Equipments" from the ProductService
+        /// and assigns them to the Products property.
         /// </summary>
-
         public void OnGet()
         {
+            // Call to the GetProductsByCategory method of the ProductService
+            // This fetches products with the specified category
             Products = ProductService.GetProductsByCategory("Snow Equipments");
         }
     }
