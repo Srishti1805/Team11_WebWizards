da43390bb842689e04ea9d4f04f5b9bcc85ef238
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2023-11-12 23:52:48 -0800

    Added new page Index_camping.cshtml.cs

---
+++ src/Pages/Product/Index_camping.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoCrafts.WebSite.Pages.Product
+{
+    public class Index_campingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
