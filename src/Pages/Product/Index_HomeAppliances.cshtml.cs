279431f08d5351edb38c81b44606c2b1f034bde8
RupaDineshKarumanchi <129802431+RupaDineshKarumanchi@users.noreply.github.com>
2023-11-13 20:49:01 -0800

    Added comments to Index_HomeAppliances.cshtml.cs

--- src/Pages/Product/Index_HomeAppliances.cshtml.cs
+++ src/Pages/Product/Index_HomeAppliances.cshtml.cs
@@ -3,17 +3,32 @@ using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 
+// Namespace for Product related to the Index page
 namespace ContosoCrafts.WebSite.Pages.Product
 {
+    /// <summary>
+    /// Index Page will return all the data to show
+    /// </summary>
     public class Index_HomeAppliancesModel : PageModel
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
         public Index_HomeAppliancesModel(JsonFileProductService productService)
         {
             ProductService = productService;
         }
+
+        // Data Service
         public JsonFileProductService ProductService { get; }
 
+        // Collection of the Data
         public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
         public void OnGet()
         {
             Products = ProductService.GetProductsByCategory("Home Appliances");
