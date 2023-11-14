43b3dd526d30d7acbb80b31f1a296d44fff69b23
RupaDineshKarumanchi <129802431+RupaDineshKarumanchi@users.noreply.github.com>
2023-11-13 20:09:20 -0800

    Added Index_HomeAppliances Model in Index_HomeAppliances.cshtml.cs

--- src/Pages/Product/Index_HomeAppliances.cshtml.cs
+++ src/Pages/Product/Index_HomeAppliances.cshtml.cs
@@ -1,11 +1,22 @@
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace ContosoCrafts.WebSite.Pages.Product
 {
     public class Index_HomeAppliancesModel : PageModel
     {
+        public Index_HomeAppliancesModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+        public JsonFileProductService ProductService { get; }
+
+        public IEnumerable<ProductModel> Products { get; private set; }
         public void OnGet()
         {
+            Products = ProductService.GetLastTwoProducts();
         }
     }
 }
