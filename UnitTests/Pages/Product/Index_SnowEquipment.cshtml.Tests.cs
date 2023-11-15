282cd1c7ef43196672b3b434b840086f74790f6b
John Vikas Kotaru <jkotaru@seattleu.edu>
2023-11-14 16:50:15 -0800

    Created unit tests in Index_SnowEquipment.cshtml.Tests.cs

---
+++ UnitTests/Pages/Product/Index_SnowEquipment.cshtml.Tests.cs
@@ -0,0 +1,104 @@
+﻿using System.Linq;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Pages.Product;
+using ContosoCrafts.WebSite.Services;
+
+namespace UnitTests.Pages.Product.Index_SnowEquipment
+{
+    /// <summary>
+    /// The IndexTests class contains the unit tests for the index page of the website
+    /// </summary>
+    public class IndexTests
+    {
+        #region TestSetup
+        public static IUrlHelperFactory urlHelperFactory;
+        public static DefaultHttpContext httpContextDefault;
+        public static IWebHostEnvironment webHostEnvironment;
+        public static ModelStateDictionary modelState;
+        public static ActionContext actionContext;
+        public static EmptyModelMetadataProvider modelMetadataProvider;
+        public static ViewDataDictionary viewData;
+        public static TempDataDictionary tempData;
+        public static PageContext pageContext;
+
+        public static Index_SnowEquipmentModel pageModel;
+
+        /// <summary>
+        /// Initialises the product services and parameters for the test
+        /// </summary>
+        [SetUp]
+        public void TestInitialize()
+        {
+            // new http context class
+            httpContextDefault = new DefaultHttpContext()
+            {
+                //RequestServices = serviceProviderMock.Object,
+            };
+
+            // new model state
+            modelState = new ModelStateDictionary();
+            // new action context
+            actionContext = new ActionContext(httpContextDefault, httpContextDefault.GetRouteData(), new PageActionDescriptor(), modelState);
+            // new model metadata provider
+            modelMetadataProvider = new EmptyModelMetadataProvider();
+            // new data dictionary view
+            viewData = new ViewDataDictionary(modelMetadataProvider, modelState);
+            // temp dictionary for data
+            tempData = new TempDataDictionary(httpContextDefault, Mock.Of<ITempDataProvider>());
+            // new page context
+            pageContext = new PageContext(actionContext)
+            {
+                ViewData = viewData,
+            };
+
+            var mockWebHostEnvironment = new Mock<IWebHostEnvironment>();
+            mockWebHostEnvironment.Setup(m => m.EnvironmentName).Returns("Hosting:UnitTestEnvironment");
+            mockWebHostEnvironment.Setup(m => m.WebRootPath).Returns("../../../../src/bin/Debug/net7.0/wwwroot");
+            mockWebHostEnvironment.Setup(m => m.ContentRootPath).Returns("./data/");
+
+            var MockLoggerDirect = Mock.Of<ILogger<Index_SnowEquipmentModel>>();
+            JsonFileProductService productService;
+            // json handler
+            productService = new JsonFileProductService(mockWebHostEnvironment.Object);
+            // new page model
+            pageModel = new Index_SnowEquipmentModel(productService)
+            {
+            };
+        }
+
+        #endregion TestSetup
+
+        /// <summary>
+        /// Test for OnGet for the index page
+        /// </summary>
+        #region OnGet
+        [Test]
+        public void OnGet_Valid_Should_Return_Product_Count()
+        {
+            // Arrange
+
+            // Act
+            pageModel.OnGet();
+            var result = pageModel.Products.ToList().Count;
+
+            // Assert
+            Assert.AreEqual(true, pageModel.ModelState.IsValid);
+            Assert.AreEqual(2, result);
+        }
+        #endregion OnGet
+    }
+}
