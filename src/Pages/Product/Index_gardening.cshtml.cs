3e0feabb12bc30de184455d89f241e2c0f83d4ca
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2023-11-17 21:20:55 -0800

    Modified Index_gardening.cshtml page

--- src/Pages/Product/Index_gardening.cshtml.cs
+++ src/Pages/Product/Index_gardening.cshtml.cs
@@ -32,7 +32,7 @@ namespace ContosoCrafts.WebSite.Pages.Product
 
         public void OnGet()
         {
-            Products = ProductService.GetProductsByCategory("Gardening Equipments");
+            Products = ProductService.GetProductsByCategory(ProductTypeEnum.GardeningEquipments);
         }
 
     }
