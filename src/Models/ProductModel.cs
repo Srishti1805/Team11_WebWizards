2573397a33a7af29952058b4238cf613aaffdae0
yashashrideshpande <45428190+yashashree98@users.noreply.github.com>
2023-11-17 21:23:07 -0800

    Modified the ProductModel.cs page

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -73,7 +73,7 @@ namespace ContosoCrafts.WebSite.Models
         /// Gets or sets the category of the product.
         /// </summary>
         [Required(ErrorMessage = "Category is required.")]
-        public string Category { get; set; }
+        public ProductTypeEnum ProductType { get; set; } = ProductTypeEnum.Undefined;
         /// <summary>
         /// Gets or sets the review of the product.
         /// </summary>
